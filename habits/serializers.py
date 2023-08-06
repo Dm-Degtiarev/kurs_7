13f4806a7acdfe3dcebde6a209114124cce92adf
Dm-Degtiarev <9774385859@mail.ru>
2023-08-06 20:09:10 +0300

    create validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Habit
+from .validators import HabitPleasureValidator, HabitRewardOrRelatedValidator, HabitTimeForExecutionValidator, \
+    HabitPeriodicityValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [
+            HabitPleasureValidator(),
+            HabitRewardOrRelatedValidator(),
+            HabitTimeForExecutionValidator(),
+            HabitPeriodicityValidator()
+        ]
